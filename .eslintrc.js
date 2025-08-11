b02cbe1d422daeb09d46ea9ff122fb8535f48ded
Vik Ntage <marindeindelike@gmail.com>
2025-08-11 08:53:12 +0300

    Setup ESLint: add .eslintrc.js and lint script

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+/** .eslintrc.js **/
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'next/core-web-vitals',
+  ],
+  rules: {
+    // Ваши кастомные правила, например:
+    // 'no-console': ['warn'],
+  },
+};
+
